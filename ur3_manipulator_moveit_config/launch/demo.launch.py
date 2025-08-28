86429664ec7840edf1e828773c70cea41a491b22
roblyat <jimbodahugga@web.de>
2025-08-28 03:19:50 +0200

    reworked jazzy moveit config

--- ur3_manipulator_moveit_config/launch/demo.launch.py
+++ ur3_manipulator_moveit_config/launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ur_manipulator", package_name="ur_manipulator_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("ur_manipulator", package_name="ur3_manipulator_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
--- ur3_manipulator_moveit_config/launch/moveit_rviz.launch.py
+++ ur3_manipulator_moveit_config/launch/moveit_rviz.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ur_manipulator", package_name="ur_manipulator_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("ur_manipulator", package_name="ur3_manipulator_moveit_config").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
